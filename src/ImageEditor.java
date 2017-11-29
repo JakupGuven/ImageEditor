6c51226d9c915e6c3e711b78c67d35c5d43abc9b
jakup <jakup@jakup-UX51VZA>
2017-11-29 17:04:12 +0100

    updated main method, completed paint method and started analyze method

--- src/ImageEditor.java
+++ src/ImageEditor.java
@@ -9,29 +9,129 @@ import javax.imageio.ImageIO;
 public class ImageEditor {
 	private static BufferedImage img;
 
+	// public static void main(String[] args) {
+	// img = paintSimpleImage();
+	// String file = args[0];
+	// try {
+	// img = ImageIO.read(new File(file));
+	// } catch (IOException e1) {
+	// // TODO Auto-generated catch block
+	// e1.printStackTrace();
+	// }
+	// if (args.length == 3) {
+	// ImageEditor.increaseContrastAndBrightness(img, Double.parseDouble(args[1]),
+	// Integer.parseInt(args[2]));
+	//
+	// } else if (args.length == 2) {
+	//// ImageEditor.makeMonochrome(img);
+	// ImageEditor.meanBlur();
+	// }
+	//
+	// try {
+	// ImageIO.write(img, "PNG", new File("painted.png"));
+	// } catch (IOException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	//
+	// }
+
 	public static void main(String[] args) {
-		String file = args[0];
-		try {
-			img = ImageIO.read(new File(file));
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		String file;
+		if (args[0].equals("p")) {
+			ImageEditor.paintSimpleImage();
+			try {
+				ImageIO.write(img, "PNG", new File("computerpainted"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else if (args[0].equals("e")) {
+			switch (args[1]) {
+			case "contrast":
+				file = args[2];
+				try {
+					img = ImageIO.read(new File(file));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				ImageEditor.changeContrast(Double.parseDouble(args[3]));
+				try {
+					ImageIO.write(img, "PNG", new File(file + "_changed_contrast"));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			case "brightness":
+				file = args[2];
+				try {
+					img = ImageIO.read(new File(file));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				ImageEditor.changeBrightness(Integer.parseInt(args[3]));
+				try {
+					ImageIO.write(img, "PNG", new File(file + "_changed_brightness"));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			case "blur":
+				file = args[2];
+				try {
+					img = ImageIO.read(new File(file));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				ImageEditor.meanBlur();
+				try {
+					ImageIO.write(img, "PNG", new File(file + "_changed_blur"));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			}
+		}else {
+			System.out.println("Unknown command: " + args[0]);
 		}
-		if (args.length == 3) {
-			ImageEditor.increaseContrastAndBrightness(img, Double.parseDouble(args[1]), Integer.parseInt(args[2]));
 
-		} else if (args.length == 2) {
-//			ImageEditor.makeMonochrome(img);
-			ImageEditor.meanBlur();
+	}
+
+	public static void paintSimpleImage() {
+		int width = 250;
+		int height = 250;
+		final int WHITE = 255;
+		final int BLACK = 0;
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+		WritableRaster raster = image.getRaster();
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				if (j % 2 == 0) {
+					raster.setSample(i, j, 0, WHITE);
+				} else {
+					raster.setSample(i, j, 0, BLACK);
+				}
+			}
 		}
 
-//		try {
-//			ImageIO.write(img, "PNG", new File("modified.png"));
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-
+		img = image;
+	}
+	
+	public static void analyzeImage() {
+		WritableRaster raster = img.getRaster();
+		for(int i = 0; i < raster.getWidth(); i++) {
+			for(int j = 0; j < raster.getHeight(); j++) {
+				for(int k = 0; k < raster.getNumBands(); k++) {
+					System.out.println(raster.getSample(i, j, k));
+				}
+			}
+		}
 	}
 
 	public static void meanBlur() {
@@ -54,32 +154,32 @@ public class ImageEditor {
 					int matrixAreaValue = 0;
 					int missedPixelCounter = 0;
 					int mI = 0, mJ = 0;
-					
+
 					for (int i = width - 1; i < width + 3; i++) {
 						for (int j = height - 1; j < height + 3; j++) {
 							try {
-//								System.out.println(filterMatrix[i][j]);
-//								System.out.println(inRaster.getSample(i, j, channels));
-//								System.out.println(missedPixelCounter);
-								matrixAreaValue += inRaster.getSample(i, j, channels) * 1;
-//								System.out.println(matrixAreaValue);
+								// System.out.println(filterMatrix[i][j]);
+								// System.out.println(inRaster.getSample(i, j, channels));
+								// System.out.println(missedPixelCounter);
+								matrixAreaValue += inRaster.getSample(i, j, channels) * 0.1;
+								// System.out.println(matrixAreaValue);
 								mJ++;
-							}catch(Exception e) {
+							} catch (Exception e) {
 								missedPixelCounter++;
 								mJ++;
 							}
 						}
 						mI++;
 					}
-//					System.out.println(matrixAreaValue);
-					int outPixelValue = matrixAreaValue/9;
-					if(outPixelValue > 255) {
-						outPixelValue = 255;
-					}else if(outPixelValue < 0) {
-						outPixelValue = 0;
-					}
+					// System.out.println(matrixAreaValue);
+					int outPixelValue = matrixAreaValue / 9;
+					// if(outPixelValue > 255) {
+					// outPixelValue = 255;
+					// }else if(outPixelValue < 0) {
+					// outPixelValue = 0;
+					// }
 					outRaster.setSample(width, height, channels, outPixelValue);
-					
+
 				}
 			}
 		}
@@ -96,15 +196,15 @@ public class ImageEditor {
 
 	}
 
-	public static void increaseContrastAndBrightness(BufferedImage inImage, double contrast, int brightness) {
-		BufferedImage image = new BufferedImage(inImage.getWidth(), inImage.getHeight(), BufferedImage.TYPE_INT_RGB);
-		WritableRaster inRaster = inImage.getRaster();
+	public static void changeContrast(double contrast) {
+		BufferedImage image = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
+		WritableRaster inRaster = img.getRaster();
 		WritableRaster outRaster = image.getRaster();
 		for (int width = 0; width < inRaster.getWidth(); width++) {
 			for (int height = 0; height < inRaster.getHeight(); height++) {
 				for (int colour = 0; colour < inRaster.getNumBands(); colour++) {
 					int rgbPixelValue = inRaster.getSample(width, height, colour);
-					rgbPixelValue = (int) (((((rgbPixelValue / 255.0) - 0.5) * contrast) + 0.5) * 255.0) + brightness;
+					rgbPixelValue = (int) (((((rgbPixelValue / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
 					if (rgbPixelValue > 255) {
 						rgbPixelValue = 255;
 					} else if (rgbPixelValue < 0) {
@@ -114,13 +214,32 @@ public class ImageEditor {
 				}
 			}
 		}
-		try {
-			ImageIO.write(image, "PNG", new File("modified.png"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		img = image;
+	}
 
+	public static void changeBrightness(int brightness) {
+		BufferedImage image = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
+		WritableRaster inRaster = img.getRaster();
+		WritableRaster outRaster = image.getRaster();
+		for (int width = 0; width < inRaster.getWidth(); width++) {
+			for (int height = 0; height < inRaster.getHeight(); height++) {
+				for (int colour = 0; colour < inRaster.getNumBands(); colour++) {
+					int rgbPixelValue = inRaster.getSample(width, height, colour);
+					if (brightness > 0) {
+						rgbPixelValue = rgbPixelValue + brightness;
+					} else {
+						rgbPixelValue = rgbPixelValue - brightness;
+					}
+					if (rgbPixelValue > 255) {
+						rgbPixelValue = 255;
+					} else if (rgbPixelValue < 0) {
+						rgbPixelValue = 0;
+					}
+					outRaster.setSample(width, height, colour, rgbPixelValue);
+				}
+			}
+		}
+		img = image;
 	}
 
 	public static void makeMonochrome(BufferedImage inImage) {
